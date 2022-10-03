92fb16e4ea7e32d96fc2267898570011d0feeb7c
dkudla <denkudla@gmail.com>
2022-10-04 02:37:19 +0300

    fixed all, but now BD check can't select July with any way. Not by index, not by text, not by elementText

--- src/test/java/com/demoqa/PracticeForm.java
+++ src/test/java/com/demoqa/PracticeForm.java
@@ -1,7 +1,7 @@
 package com.demoqa;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -20,68 +20,87 @@ public class PracticeForm {
 
     @BeforeAll
     static void configure() {
+        System.out.println("### @BeforeAll");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
            }
 
     @Test
-    void fillFormTest() throws InterruptedException {
+    void fillForm() {
         open("/automation-practice-form");
-        //Delete banner
+    //Delete banner
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
-        //Set name
+    //Set name
+        System.out.println("###     @fillName() !");
         $("#firstName").setValue("Denis");
-        //Set surname
+    //Set surname
+        System.out.println("###     @fillSurname() !");
         $("#lastName").setValue("Kudla");
-        //Set email
+    //Set email
+        System.out.println("###     @fillEmail");
         $("#userEmail-wrapper #userEmail").setValue("denis@kudla.com");
-        //Set gender
+    //Set gender
+        System.out.println("###     @fillGender");
         $("#genterWrapper").$(byText("Male")).click();
-        //Set phone number
+    //Set phone number
+        System.out.println("###     @fillNumber");
         $("#userNumber-wrapper #userNumber").setValue("7777777777");
-        //Set birth date
+    //Set birth date
+        System.out.println("###     @fillBirthDate");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1994");
-        $(By.xpath("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[5]/div[6]")).click();
-        //Set objects
-        //X_X
-        //Set hobbies
+        $(".react-datepicker__month-select").selectOption(6);
+        $(".react-datepicker__day--029").click();
+    //Set Subjects
+        $("#subjectsInput").setValue("Social").pressEnter();
+    //Set hobbies
+        System.out.println("###     @fillHobbies");
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
-        //Upload image
+    //Upload image
+        System.out.println("###     @uploadImage");
         $("#uploadPicture").uploadFile(new File("src/test/resources/1617343839_50-p-oboi-osennii-sad-59.jpeg"));
-        //Set adress
+    //Set address
+        System.out.println("###     @fillAddress");
         $("#currentAddress").setValue("st.Pushkina 6, bld. Kolotuskina 9, app 42");
-        //More adress
 
+    //Set State and City
+        System.out.println("###     @fillState and City");
+        $("#state").click();
+        $("#state").$(byText("NCR")).click();
+
+        $("#city").click();
+        $("#city").$(byText("Delhi")).click();
+
+
+    //Now lets check this out
         $("#submit").click();
 
 
-        //Now lets check
-
-        //Student Name
+    //Student Name
         $$(".table-responsive").findBy(text("Student Name")).shouldHave(text("Denis Kudla"));
-        //Student Email
+    //Student Email
         $$(".table-responsive").findBy(text("Student Email")).shouldHave(text("denis@kudla.com"));
-        //Gender
+    //Gender
         $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Male"));
-        //Mobile
+    //Mobile
         $$(".table-responsive").findBy(text("Mobile")).shouldHave(text("7777777777"));
-        //Date of Birth
+    //Date of Birth
         $$(".table-responsive").findBy(text("Date of Birth")).shouldHave(text("29 July,1994"));
-        //Subjects
-        //  I could't find the way to check this -_-
+    //Subjects
+        $$(".table-responsive").findBy(text("Subjects")).shouldHave(text("Social Studies"));
         //Hobbies
         $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Sports, Reading, Music"));
-        //Picture
+    //Picture
         $$(".table-responsive").findBy(text("Picture")).shouldHave(text("1617343839_50-p-oboi-osennii-sad-59.jpeg"));
-        //Address
+    //Address
         $$(".table-responsive").findBy(text("Address")).shouldHave(text("st.Pushkina 6, bld. Kolotuskina 9, app 42"));
+    //State and City
+        $$(".table-responsive").findBy(text("State and City")).shouldHave(text("NCR Delhi"));
 
-        //Close tab
+    //Close tab
         $("#closeLargeModal").click();
 
     }
